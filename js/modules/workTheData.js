2d027b510f1eebda2965746ba6e2346f97791c8b
Andrei Tudor <andrei.s.tudor@gmail.com>
2018-07-17 18:24:22 +0300

    data fetch with relative paths

--- js/modules/workTheData.js
+++ js/modules/workTheData.js
@@ -8,7 +8,7 @@ import drawGraph from '../drawGraph.js'
 const workTheData = (config) => {
 
 
-  d3.csv(`/data/${config.file}.csv`)
+  d3.csv(`../../data/${config.file}.csv`)
     // .then( data => { return data } )  in consola imi da "data" deja transformat de fctia care este in then-ul urmator!! WHY ?!?!
     .then( convertStringsToNumbers ) // TODO: catch if data contains NaN
     .then( drawTable(config) )
